72f05542f60bfb4431b4dc482d9472d484ad2d07
Zibrian Cadinot <zcadinot@c1r5p11.42lehavre.fr>
2026-03-18 15:52:06 +0100

    Finish popen

--- rank4/ft_popen/ft_popen.c
+++ rank4/ft_popen/ft_popen.c
@@ -5,20 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: zcadinot <zcadinot@student.42lehavre.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/18 15:22:04 by zcadinot          #+#    #+#             */
-/*   Updated: 2026/03/18 15:40:24 by zcadinot         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_popen.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: zcadinot <zcadinot@student.42lehavre.      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/18 13:23:06 by zcadinot          #+#    #+#             */
-/*   Updated: 2026/03/18 14:44:58 by zcadinot         ###   ########.fr       */
+/*   Created: 2026/03/18 15:50:53 by zcadinot          #+#    #+#             */
+/*   Updated: 2026/03/18 15:51:59 by zcadinot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +17,8 @@
 int ft_popen(const char *file, char *const argv[], char type)
 {
     if (!argv || !file || (type != 'r' && type != 'w'))
-    {
         return (-1);
-    }
+
     int pipefd[2];
     pid_t pid;
 
@@ -40,35 +27,28 @@ int ft_popen(const char *file, char *const argv[], char type)
 
     pid = fork();
     if (pid == -1)
-    {
         return (-1);
-    }
     if (pid == 0)
     {
         if (type == 'r')
         {
             if (dup2(pipefd[1], 1) == -1)
                 exit(-1);
-            close(pipefd[0]);
-            close(pipefd[1]);
-            execvp(file, argv);
-            exit(1);
         }
         if (type == 'w')
         {
             if (dup2(pipefd[0], 0) == -1)
                 exit(-1);
-            close(pipefd[1]);
-            close(pipefd[0]);
-            execvp(file, argv);
-            exit(1);
-        }
         else
         {
             close(pipefd[1]);
             close(pipefd[0]);
             exit(-1);
         }
+        close(pipefd[1]);
+        close(pipefd[0]);
+        execvp(file, argv);
+        exit(1);
     }
     else
     {
