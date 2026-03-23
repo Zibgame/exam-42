b96fde62a895b848f13575784df067c2dd2c0566
Zibgame <fanizicy@gmail.com>
2026-03-23 13:45:46 -0400

    Fix popen

--- rank4/ft_popen/ft_popen.c
+++ rank4/ft_popen/ft_popen.c
@@ -6,7 +6,7 @@
 /*   By: zcadinot <zcadinot@student.42lehavre.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 15:50:53 by zcadinot          #+#    #+#             */
-/*   Updated: 2026/03/18 15:51:59 by zcadinot         ###   ########.fr       */
+/*   Updated: 2026/03/23 13:43:11 by zcadinot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,45 +16,51 @@
 
 int ft_popen(const char *file, char *const argv[], char type)
 {
-    if (!argv || !file || (type != 'r' && type != 'w'))
+    if (!file || !argv || (type != 'w' && type != 'r'))
         return (-1);
-
     int pipefd[2];
-    pid_t pid;
-
+    pid_t pid = -1;
     if (pipe(pipefd) == -1)
+    {
         return (-1);
-
+    }
     pid = fork();
     if (pid == -1)
+    {
         return (-1);
+    }
     if (pid == 0)
     {
         if (type == 'r')
         {
             if (dup2(pipefd[1], 1) == -1)
+            {
                 exit(-1);
+            }
         }
-        if (type == 'w')
+        else if(type == 'w')
         {
             if (dup2(pipefd[0], 0) == -1)
+            {
                 exit(-1);
+            }
+        }
         else
         {
-            close(pipefd[1]);
             close(pipefd[0]);
+            close(pipefd[1]);
             exit(-1);
         }
-        close(pipefd[1]);
         close(pipefd[0]);
-        execvp(file, argv);
+        close(pipefd[1]);
+        execvp(file,argv);
         exit(1);
     }
     else
     {
         if (type == 'r')
         {
-            close(pipefd[1]);
+            close (pipefd[1]);
             return (pipefd[0]);
         }
     }
@@ -62,3 +68,21 @@ int ft_popen(const char *file, char *const argv[], char type)
     return (pipefd[1]);
 }
 
+int main(void)
+{
+    int fd;
+    char buf[256];
+    int r;
+
+    fd = ft_popen("ls", (char *const []){"ls", NULL}, 'r');
+    if (fd == -1)
+        return (1);
+
+    r = read(fd, buf, 255);
+    if (r > 0)
+    {
+        buf[r] = '\0';
+        printf("%s", buf);
+    }
+    return (0);
+}
