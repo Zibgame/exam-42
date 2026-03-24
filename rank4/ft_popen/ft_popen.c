5515a5a18e2e3956ec9bbf7e72c100a4feb3f263
Zibrian Cadinot <zcadinot@c1r5p9.42lehavre.fr>
2026-03-24 16:19:24 +0100

    Finish picosheel

--- rank4/ft_popen/ft_popen.c
+++ rank4/ft_popen/ft_popen.c
@@ -6,7 +6,7 @@
 /*   By: zcadinot <zcadinot@student.42lehavre.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 15:50:53 by zcadinot          #+#    #+#             */
-/*   Updated: 2026/03/23 13:43:11 by zcadinot         ###   ########.fr       */
+/*   Updated: 2026/03/24 13:21:11 by zcadinot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,14 @@
 
 int ft_popen(const char *file, char *const argv[], char type)
 {
-    if (!file || !argv || (type != 'w' && type != 'r'))
+    if (!file || !argv || (type != 'r' && type != 'w'))
+    {
         return (-1);
+    }
+
     int pipefd[2];
     pid_t pid = -1;
+
     if (pipe(pipefd) == -1)
     {
         return (-1);
@@ -33,17 +37,13 @@ int ft_popen(const char *file, char *const argv[], char type)
     {
         if (type == 'r')
         {
-            if (dup2(pipefd[1], 1) == -1)
-            {
+            if (dup2(pipefd[0],0) == -1)
                 exit(-1);
-            }
         }
-        else if(type == 'w')
+        else if (type == 'w')
         {
-            if (dup2(pipefd[0], 0) == -1)
-            {
+            if (dup2(pipefd[1],1) == -1)
                 exit(-1);
-            }
         }
         else
         {
@@ -51,21 +51,21 @@ int ft_popen(const char *file, char *const argv[], char type)
             close(pipefd[1]);
             exit(-1);
         }
-        close(pipefd[0]);
-        close(pipefd[1]);
-        execvp(file,argv);
-        exit(1);
+            close(pipefd[0]);
+            close(pipefd[1]);
+            execvp(file,argv);
+            exit(-1);
     }
     else
     {
         if (type == 'r')
         {
-            close (pipefd[1]);
-            return (pipefd[0]);
+            close(pipefd[0]);
+            return (pipefd[1]);
         }
     }
-    close(pipefd[0]);
-    return (pipefd[1]);
+    close(pipefd[1]);
+    return (pipefd[0]);
 }
 
 int main(void)
--- rank4/picoshell/picoshell.c
+++ rank4/picoshell/picoshell.c
@@ -6,7 +6,7 @@
 /*   By: zcadinot <zcadinot@student.42lehavre.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 16:13:14 by zcadinot          #+#    #+#             */
-/*   Updated: 2026/03/24 14:02:11 by zcadinot         ###   ########.fr       */
+/*   Updated: 2026/03/24 16:18:59 by zcadinot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,49 +16,72 @@
 int picoshell(char **cmds[])
 {
     if (!cmds)
-        return (-1);
+        return (1);
+
     int pipefd[2];
-    int lastfd = -1;
-
-    pid_t pid = -1;
+    pid_t pid;
+    int last_fd = -1;
     
-    while (cmds[i])
+    while (cmd[i])
     {
-        if (cmds[i + 1] && pipe(pipefd) == -1);
+        if (!cmd[i + 1] && pipe(pipefd) == -1) 
+        {
+            if (last_fd != -1)
+            {
+                close(last_fd);
+            }
             return (1);
+        }
 
-        pid = fork();
+        pid = fork()
         if (pid == -1)
         {
-            if (cmds[i + 1])
+            if (cmd[i + 1])
             {
-                close(pipefd[1]);
+                close(pipefd[0]);
                 close(pipefd[0]);
             }
-            if (lastfd == -1)
+            if (last_fd != -1)
             {
-                close(lastfd);
+                close(last_fd);
             }
-            return (1)
+            return (1);
         }
-
         if (pid == 0)
-        {   
-            execvp(cmd[i][0],cmd[i])
-            exit(1);
-        }
-        if (lastfd != -1)
         {
-            close(lastfd);
+            if (last_fd != -1)
+            {
+                if (dup2(last_fd, 0) == -1)
+                {
+                    close(last_fd);
+                    exit(0);
+                }
+                close(last_fd);
+            }
+            if (cmd[i + 1])
+            {
+                close(fd[0])
+                if (dup2(pipefd[1], 1) == -1)
+                {
+                    close(pipefd[1]);
+                    exit(0);
+                }
+                close(pipefd[1]);
+            }
         }
-        if (cmds[i + 1])
-        {
-            close(pipefd[1]);
-            lastfd = pipefd[0];
-        }
-        i++;
+        execvp(cmds[i][0], cmd[i]);
+        exit(1);
     }
-    while (wait(NULL) > 0)
+    if (last_fd != -1)
+    {
+        close(last_fd);
+    }
+    if (cmd[i + 1])
+    {
+        close(pipefd[1]);
+        last_fd = pipefd[0];
+    }
+    while (wait(null) > 0)
         ;
     return (0);
 }
