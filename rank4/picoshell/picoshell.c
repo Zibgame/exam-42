c2e40c390d3be2ce11f27792b00d20e82ed62994
Zibgame <fanizicy@gmail.com>
2026-03-25 13:30:28 -0400

    Modif picoshell

--- rank4/picoshell/picoshell.c
+++ rank4/picoshell/picoshell.c
@@ -6,89 +6,127 @@
 /*   By: zcadinot <zcadinot@student.42lehavre.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 16:13:14 by zcadinot          #+#    #+#             */
-/*   Updated: 2026/03/24 16:18:59 by zcadinot         ###   ########.fr       */
+/*   Updated: 2026/03/25 13:29:44 by zcadinot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
 #include <stdlib.h>
 #include <unistd.h>
 
-int picoshell(char **cmds[])
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+static void	exec_cmd(char **cmd, int in, int out)
 {
-    if (!cmds)
-        return (1);
-
-    int pipefd[2];
-    pid_t pid;
-    int last_fd = -1;
-    
-    while (cmd[i])
-    {
-        if (!cmd[i + 1] && pipe(pipefd) == -1) 
-        {
-            if (last_fd != -1)
-            {
-                close(last_fd);
-            }
-            return (1);
-        }
-
-        pid = fork()
-        if (pid == -1)
-        {
-            if (cmd[i + 1])
-            {
-                close(pipefd[0]);
-                close(pipefd[0]);
-            }
-            if (last_fd != -1)
-            {
-                close(last_fd);
-            }
-            return (1);
-        }
-        if (pid == 0)
-        {
-            if (last_fd != -1)
-            {
-                if (dup2(last_fd, 0) == -1)
-                {
-                    close(last_fd);
-                    exit(0);
-                }
-                close(last_fd);
-            }
-            if (cmd[i + 1])
-            {
-                close(fd[0])
-                if (dup2(pipefd[1], 1) == -1)
-                {
-                    close(pipefd[1]);
-                    exit(0);
-                }
-                close(pipefd[1]);
-            }
-        }
-        execvp(cmds[i][0], cmd[i]);
-        exit(1);
-    }
-    if (last_fd != -1)
-    {
-        close(last_fd);
-    }
-    if (cmd[i + 1])
-    {
-        close(pipefd[1]);
-        last_fd = pipefd[0];
-    }
-    while (wait(null) > 0)
-        ;
-    return (0);
+	if (in != -1)
+	{
+		dup2(in, 0);
+		close(in);
+	}
+	if (out != -1)
+	{
+		dup2(out, 1);
+		close(out);
+	}
+	execvp(cmd[0], cmd);
+	exit(1);
 }
 
-int main()
+int	picoshell(char **cmds[])
 {
-    char **cmds;
-    picoshell(&cmds);
-    return (0);
+	int		i;
+	int		fd[2];
+	int		last;
+	pid_t	pid;
+
+	i = 0;
+	last = -1;
+	while (cmds[i])
+	{
+		if (cmds[i + 1])
+			pipe(fd);
+		pid = fork();
+		if (pid == 0)
+		{
+			if (cmds[i + 1])
+				exec_cmd(cmds[i], last, fd[1]);
+			else
+				exec_cmd(cmds[i], last, -1);
+		}
+		if (last != -1)
+			close(last);
+		if (cmds[i + 1])
+		{
+			close(fd[1]);
+			last = fd[0];
+		}
+		i++;
+	}
+	while (wait(NULL) > 0)
+		;
+	return (0);
+}
+
+static int	count_cmds(int ac, char **av)
+{
+	int	i;
+	int	count;
+
+	i = 1;
+	count = 1;
+	while (i < ac)
+	{
+		if (av[i][0] == '|' && av[i][1] == '\0')
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+static char	***build_cmds(int ac, char **av)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	***cmds;
+
+	cmds = malloc(sizeof(char **) * (count_cmds(ac, av) + 1));
+	if (!cmds)
+		return (NULL);
+	i = 1;
+	k = 0;
+	while (i < ac)
+	{
+		j = i;
+		while (j < ac && !(av[j][0] == '|' && av[j][1] == '\0'))
+			j++;
+		cmds[k] = &av[i];
+		if (j < ac)
+			av[j] = NULL;
+		i = j + 1;
+		k++;
+	}
+	cmds[k] = NULL;
+	return (cmds);
+}
+
+int	main(int ac, char **av)
+{
+	char	***cmds;
+	int		ret;
+
+	if (ac < 2)
+		return (1);
+	cmds = build_cmds(ac, av);
+	if (!cmds)
+		return (1);
+	ret = picoshell(cmds);
+	free(cmds);
+	return (ret);
 }
