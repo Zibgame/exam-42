ad1312aaddf910c3ee662442c6b127c3d69daf8d
Zibgame <zibgame@Zibgame.Zibgame>
2026-08-18 17:29:04 -0400

    Update bsq

--- rank5/Level1/Bsq/bsq.c
+++ rank5/Level1/Bsq/bsq.c
@@ -6,14 +6,38 @@
 /*   By: zcadinot <zcadinot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/18 16:47:49 by zcadinot          #+#    #+#             */
-/*   Updated: 2026/08/18 16:50:08 by zcadinot         ###   ########.fr       */
+/*   Updated: 2026/08/18 17:27:43 by zcadinot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bsq.h"
 
+int do_bsq(FILE *file)
+{
+    return (0);
+}
+
+int do_bsq_path(char *file)
+{
+    return (0);
+}
+
 int main(int argc, char **argv)
 {
-    printf("Hello World BSQ running ...");
+    printf("BSQ running ... \n");
+    if (argc == 1)
+    {
+        printf("stdin\n");
+    }
+    else if (argc > 1)
+    {
+        size_t i = 0;
+
+        while (i < argc - 1)
+        {
+            do_bsq_path(argv[i]);
+            i++;
+        }
+    }
     return (0);
 }
