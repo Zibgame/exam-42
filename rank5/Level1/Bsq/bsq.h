6ff94668f6feaa8c3d9af5ca102e3809b4a7503a
Zibrian Cadinot <zcadinot@c2r6p6.42lehavre.fr>
2026-09-10 15:13:47 +0200

    Delete a.out

--- rank5/Level1/Bsq/bsq.h
+++ rank5/Level1/Bsq/bsq.h
@@ -6,7 +6,7 @@
 /*   By: zcadinot <zcadinot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/18 16:48:31 by zcadinot          #+#    #+#             */
-/*   Updated: 2026/08/19 01:30:36 by zcadinot         ###   ########.fr       */
+/*   Updated: 2026/09/10 13:46:16 by zcadinot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,23 @@
 
 typedef struct s_map
 {
-    char **map;
+    char **grid;
     int width;
     int height;
 } t_map;
 
 typedef struct s_elt
 {
-    int n_ligne;
+    int row;
     char empty;
     char obstacle;
     char full;
 } t_elt;
+
+typedef struct s_sqr
+{
+    int x;
+    int y;
+    int height;
+    int width;
+} t_sqr;
